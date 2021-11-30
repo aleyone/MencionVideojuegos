e34a227ce6f6c0344d14d4c2a061b090a3ec930d
David Gil <dagipe01@floridauniversitaria.es>
2021-12-01 00:08:12 +0100

    Commit Fighter + GameManager

---
+++ Fighter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public string Name;
+    public string Category;
+    public int Health;
+       
+    public void DecreaseHealth(int masVida)
+    {
+        Health -= masVida;
+    }
+
+    public bool isAlive()
+    {
+        bool alive = true;
+        if (Health == 0)
+        {
+            alive = false;
+        }
+
+        return alive;
+    }
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    string[] nombre = { "Escorpion", "Kano", "Sonya", "Subzero", "Johnny Cage" };
+    string[] categoria = { "Ninja", "Mercenario", "Teniente", "Ninja", "Actor" };
+    GameObject[] e;
+    List<GameObject> Deaths = new List<GameObject>();
+    Fighter luchador;
+
+    public int aleatHealth()
+    {
+        int vida = (int)Random.Range(80, 100);
+        return vida;
+    }
+    public void InitFighter(GameObject[] e)
+    {
+        
+        for (int i = 0; i < e.Length; i++)
+        {
+            e[i].GetComponent<Fighter>().Name = nombre[i];
+            e[i].GetComponent<Fighter>().Category = categoria[i];
+            e[i].GetComponent<Fighter>().Health = aleatHealth();
+        }
+    }
+
+    public GameObject maxLife(GameObject[] e) 
+    {
+        int maximo=0;
+        GameObject max=e[0];
+        for (int i=0; i< e.Length; i++)
+        {
+            Debug.Log("Entra " + e[i].GetComponent<Fighter>().Name + " al juego");
+            if (e[i].GetComponent<Fighter>().Health >= maximo)
+            {                
+                maximo = e[i].GetComponent<Fighter>().Health;
+                max = e[i];
+                Debug.Log("Ahora el maximo es: " + max.GetComponent<Fighter>().Name + " con " + max.GetComponent<Fighter>().Health);
+            }
+
+
+        }
+        
+
+        return max;
+    }
+
+    public void addDeath(GameObject f)
+    {
+        Deaths.Add(f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        e = GameObject.FindGameObjectsWithTag("tagFighter");
+        InitFighter(e);
+        maxLife(e);
+        luchador = new Fighter();
+        Debug.Log("Vamos a matar a " + e[3].GetComponent<Fighter>().Name);
+        e[3].GetComponent<Fighter>().Health = 0;
+        
+        Debug.Log("Recorremos lista Deaths");
+        foreach (GameObject muerto in Deaths)
+        {
+            Debug.Log(muerto.GetComponent<Fighter>().Name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0; i< e.Length; i++)
+        {
+            if (e[i].GetComponent<Fighter>().isAlive())
+            {
+                Deaths.Add(e[i]);
+            }
+        }
+        
+    }
+}
