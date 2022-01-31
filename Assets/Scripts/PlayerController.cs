9b6538c2bdd396f134527cb870ac53e6333c2a32
David Gil <dagipe01@floridauniversitaria.es>
2022-01-31 19:48:03 +0100

    Ejercicio 4

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D PlayerRB;
+    float fuerza=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float movimiento = Input.GetAxis("Horizontal");
+        PlayerRB.velocity = (transform.right * movimiento * fuerza);
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
