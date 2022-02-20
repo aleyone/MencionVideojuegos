ee140ae5693e4277c2015a445e05d1341cdb55a3
David Gil <dagipe01@floridauniversitaria.es>
2022-02-20 18:51:27 +0100

    Commit ejercicio 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D PlayerRB;
     SpriteRenderer PlayerSR;
     float fuerza=3;
+    Animator run;
     // Start is called before the first frame update
     void Start()
     {
         PlayerRB = GetComponent<Rigidbody2D>();
         PlayerSR = GetComponent<SpriteRenderer>();
+        run = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -20,6 +22,7 @@ public class PlayerController : MonoBehaviour
 
 
         PlayerRB.velocity = (transform.right * movimiento * fuerza);
+        run.SetFloat("MoveSpeed", Mathf.Abs(movimiento));
         if (movimiento >= 0)
         {            
             PlayerSR.flipX = false;
