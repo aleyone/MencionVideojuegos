7cd379b4f4ddac87c9939dbfcaf825b23fbf9c09
David Gil <dagipe01@floridauniversitaria.es>
2022-02-21 20:03:15 +0100

    Commit ejercicio salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,58 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D PlayerRB;
     SpriteRenderer PlayerSR;
     float fuerza=3;
-    Animator run;
+    Animator anim;
+    public float jumpForce;
+    bool isGrounded=true;
     // Start is called before the first frame update
     void Start()
     {
         PlayerRB = GetComponent<Rigidbody2D>();
         PlayerSR = GetComponent<SpriteRenderer>();
-        run = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+        Physics2D.queriesStartInColliders = false;
     }
 
     private void FixedUpdate()
     {
         float movimiento = Input.GetAxis("Horizontal");
+        anim.SetFloat("VerticalVelocity", PlayerRB.velocity.y);
+        Debug.Log("Velocidad en y " + PlayerRB.velocity.y);
+
+        if (Input.GetAxis("Jump") > 0 && isGrounded==true)
+
+        {
+            Debug.Log("salto");
+            PlayerRB.velocity = new Vector2(PlayerRB.velocity.x, 0);
+            PlayerRB.AddForce(new Vector2(0, jumpForce));
+            //PlayerRB.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+            isGrounded = false;
+            anim.SetBool("isGrounded", false);
 
 
-        PlayerRB.velocity = (transform.right * movimiento * fuerza);
-        run.SetFloat("MoveSpeed", Mathf.Abs(movimiento));
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+
+        // If it hits something...
+        if (hit.collider != null && isGrounded==false && PlayerRB.velocity.y <0)
+        {
+            if(hit.distance < 0.5)
+            {
+                Debug.Log("Detecto " + hit.collider.gameObject.name + "a distancia" + hit.distance);
+                isGrounded = true;
+                anim.SetBool("isGrounded", true);
+            }
+        }
+    
+
+
+
+
+
+    PlayerRB.velocity = (transform.right * movimiento * fuerza);
+        anim.SetFloat("MoveSpeed", Mathf.Abs(movimiento));
         if (movimiento >= 0)
         {            
             PlayerSR.flipX = false;
